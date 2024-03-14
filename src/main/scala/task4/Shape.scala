2b4ef4a81c591e7baafef8d64f1be07fd2b658bf
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-03-14 18:56:22 +0100

    Add area calculation implementation

--- src/main/scala/task4/Shape.scala
+++ src/main/scala/task4/Shape.scala
@@ -6,6 +6,11 @@ enum Shape:
   case Circle(radius: Double)
 
 def perimeter(shape: Shape): Double = shape match
-    case Shape.Square(side) => side * 4
-    case Shape.Rectangle(majorSide, minorSide) => (majorSide + minorSide) * 2
-    case Shape.Circle(radius) => 2 * Math.PI * radius
+  case Shape.Square(side) => side * 4
+  case Shape.Rectangle(majorSide, minorSide) => (majorSide + minorSide) * 2
+  case Shape.Circle(radius) => 2 * Math.PI * radius
+
+def area(shape: Shape): Double = shape match
+  case Shape.Square(side) => side * side
+  case Shape.Rectangle(majorSide, minorSide) => majorSide * minorSide
+  case Shape.Circle(radius) => Math.PI * radius * radius
