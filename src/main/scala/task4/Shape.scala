12825e123125c0c3a82f331c6ece1c7d9f895793
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-03-14 18:53:53 +0100

    Add shapes implementation

---
+++ src/main/scala/task4/Shape.scala
@@ -0,0 +1,11 @@
+package task4
+
+enum Shape:
+  case Square(side: Double)
+  case Rectangle(majorSide: Double, minorSide: Double)
+  case Circle(radius: Double)
+
+def perimeter(shape: Shape): Double = shape match
+    case Shape.Square(side) => side * 4
+    case Shape.Rectangle(majorSide, minorSide) => (majorSide + minorSide) * 2
+    case Shape.Circle(radius) => 2 * Math.PI * radius
