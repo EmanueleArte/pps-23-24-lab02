7a621be58be1af0b68bffb1663e80bb1f18cc9f5
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-03-14 18:59:31 +0100

    Add shape scale implementation

--- src/main/scala/task4/Shape.scala
+++ src/main/scala/task4/Shape.scala
@@ -14,3 +14,8 @@ def area(shape: Shape): Double = shape match
   case Shape.Square(side) => side * side
   case Shape.Rectangle(majorSide, minorSide) => majorSide * minorSide
   case Shape.Circle(radius) => Math.PI * radius * radius
+
+def scale(shape: Shape, alpha: Double): Shape = shape match
+  case Shape.Square(side) => Shape.Square(side * alpha)
+  case Shape.Rectangle(majorSide, minorSide) => Shape.Rectangle(majorSide * alpha, minorSide * alpha)
+  case Shape.Circle(radius) => Shape.Circle(radius * alpha)
