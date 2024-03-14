b65fdaf6ac48d5e1c3a79b3a2476ce486b189c92
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-03-14 18:40:30 +0100

    Create RecursionTest

---
+++ src/test/scala/task3/RecursionTest.scala
@@ -0,0 +1,19 @@
+package task3
+
+import org.junit.*
+import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions.assertAll
+
+class RecursionTest:
+
+  @Test def testGcdEqualNumbers(): Unit =
+    assertEquals(5, gcd(5, 5))
+
+  @Test def testGcdWithOneNumberIsZero(): Unit =
+    assertEquals(5, gcd(0, 5))
+
+  @Test def testGcdWithBothNumbersAreZero(): Unit =
+    assertThrows(classOf[ArithmeticException], () => gcd(0, 0))
+
+  @Test def testGcdWithBothNumbers(): Unit =
+    assertEquals(5, gcd(5, 10))
