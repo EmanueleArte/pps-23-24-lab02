55dfa88cf428c58fa736d1c47702bb59e588f110
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-03-14 14:47:01 +0100

    Add tests for generic compose

--- src/test/scala/task2b/FunctionalCompositionTest.scala
+++ src/test/scala/task2b/FunctionalCompositionTest.scala
@@ -10,3 +10,19 @@ class FunctionalCompositionTest:
     val g: Int => Int = (x: Int) => x * 2
     val composed: Int = intCompose(f, g)(5)
     assertEquals(9, composed)
+
+  @Test def genericMethodCompositionForStringTest(): Unit =
+    val composed: String = compose("hello " + _, "" + _)("world")
+    assertEquals("hello world", composed)
+
+  @Test def genericMethodCompositionForIntTest(): Unit =
+    val f: Int => Int = (x: Int) => x - 1
+    val g: Int => Int = (x: Int) => x * 2
+    val composed: Int = compose(f, g)(5)
+    assertEquals(9, composed)
+
+  @Test def genericMethodCompositionForIntAndBooleanTest(): Unit =
+    val f: Int => Boolean = (x: Int) => x > 0
+    val g: Int => Int = (x: Int) => x + 1
+    val composed: Boolean = compose(f, g)(5)
+    assertTrue(composed)
